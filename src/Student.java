8f2a6f0f30107a19122d337c6daa4ba7a32a7a00
Clayton <cmpetersen@hotmail.co.za>
2019-09-21 08:50:44 +0200

    adding an arraylist

--- src/Student.java
+++ src/Student.java
@@ -1,19 +1,39 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Student {
 
-    private ArrayList<String> studentNames = new ArrayList();
+    private static ArrayList<String> studentNames = new ArrayList();
 
     public static void main(String args[]){
 
+        addStudents(studentNames);
+        displayStudents(studentNames);
+        Collections.sort(studentNames);
+        displayStudents(studentNames);
 
+    }//end of main
+
+    public static void addStudents(ArrayList<String> studentNames){
+
+        studentNames.add("Mark");
+        studentNames.add("Andrew");
+        studentNames.add("Rachel");
+
+    }//End of addStudents
+
+    public static void displayStudents(ArrayList<String> studentNames){
+
+        for(String student: studentNames){
+
+            System.out.println("Name: " + student);
+
+        }
+
+    }//end of displayStudents
 
     }
 
-    public static void addStudents(){
 
 
-
-    }
-
-}
+}//end of class
