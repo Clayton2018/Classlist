0f84734bc35687350e2c2362edb6137b1a30ad74
Clayton <cmpetersen@hotmail.co.za>
2019-09-21 08:41:24 +0200

    second commit

---
+++ src/Student.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Student {
+
+    private ArrayList<String> studentNames = new ArrayList();
+
+    public static void main(String args[]){
+
+
+
+    }
+
+    public static void addStudents(){
+
+
+
+    }
+
+}
